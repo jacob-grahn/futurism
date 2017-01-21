454b06933bfaa185418bb8a1c969a7bf08685925
Jacob Grahn <contact@jacobgrahn.com>
2017-01-20 21:16:54 -0500

    popup mode doesn't seem to be required for logins to work, it seems like going without is the smoother experience

--- src/pages/login-page/firebaseui-config.js
+++ src/pages/login-page/firebaseui-config.js
@@ -3,19 +3,15 @@ import router from '../../services/router'
 
 export default {
   callbacks: {
-    // Called when the user has been successfully signed in.
     signInSuccess: function (user, credential, redirectUrl) {
       router.replace('/splash')
       // Do not redirect.
       return false
     }
   },
-  // Opens IDP Providers sign-in flow in a popup.
-  signInFlow: 'popup',
   signInOptions: [
     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
     firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-    // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
     firebase.auth.GithubAuthProvider.PROVIDER_ID,
     firebase.auth.EmailAuthProvider.PROVIDER_ID
   ],
