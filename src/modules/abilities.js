a6a246704a410519dd6baea7d0c294b46cc403e1
Jacob Grahn <contact@jacobgrahn.com>
2017-02-23 00:32:23 -0500

    Play around with how some of these abilities might work. Trying to give functional programming a go with mixed success

---
+++ src/modules/abilities.js
@@ -0,0 +1,76 @@
+// this data should probably be in a databse
+// it will always be used, so it's a bit easier to have it here
+
+import { map, prop, add, subtract, pipe, max } from 'ramda'
+
+const attackEffect = ({ target, level }) => {
+  pipe(
+    prop('hp'),
+    subtract(level),
+    max(0)
+  )(target)
+}
+
+export default {
+  attack: {
+    name: 'Attack',
+    cost: 1,
+    canLevel: true,
+    activateOn: 'swipe-in',
+    effect: attackEffect
+  },
+  hp: {
+    name: 'HP',
+    cost: 0.5,
+    canLevel: true,
+    activateOn: null,
+    effect: () => {}
+  },
+  protect: {
+    name: 'Protect',
+    cost: 2,
+    canLevel: true,
+    activateOn: 'passive',
+    effect: ({ neighbors, level }) => {
+      map(
+        pipe(
+          prop('sheild'),
+          add(level)
+        ),
+        neighbors
+      )
+    }
+  },
+  'counter-attack': {
+    name: 'Counter Attack',
+    cost: 1,
+    canLevel: true,
+    activateOn: 'post-swipe-in',
+    effect: attackEffect
+  },
+  slippery: {
+    name: 'Slippery',
+    cost: 2,
+    canLevel: false,
+    activateOn: 'pre-swipe-in',
+    effect: ({ deck, self }) => {
+      pipe(
+        prepend(self)
+        takeLast(1)
+        set(
+          lense('position'),
+          view(lense('position'), self)
+        )
+      )(deck)
+      deck = withoutLast(1, deck)
+      self = without('position', self)
+    }
+  },
+  poison: {},
+  upgrade: {},
+  quick: {},
+  heal: {},
+  zombie: {},
+  shapeshift: {},
+  hidden: {}
+}
