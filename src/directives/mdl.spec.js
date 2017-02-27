f8626e669566d00bbbbc981e36b4ced4320971c6
Jacob Grahn <contact@jacobgrahn.com>
2017-02-26 19:26:03 -0500

    add unit test

---
+++ src/directives/mdl.spec.js
@@ -0,0 +1,30 @@
+/* global expect, it, describe, beforeEach, afterEach */
+
+import Vue from 'vue'
+// import * as compiler from 'vue-template-compiler'
+import mdl from './mdl'
+// import { createComponent } from '../../test/unit/specs/setup'
+
+describe('mdl.js', () => {
+  beforeEach(() => {
+    window.componentHandler = {
+      upgradeElement: (el) => {
+        console.log('upgradeElement', el)
+        el.innerHTML = 'Upgraded'
+      }
+    }
+  })
+
+  afterEach(() => {
+    delete window.componentHandler
+  })
+
+  it('upgrade mdl elements after they have been added to the dom', () => {
+    const vm = new Vue({
+      render: (h) => h('button', {directives: [{name: 'mdl'}]}),
+      directives: {mdl}
+    }).$mount()
+    expect(vm.$el.textContent).to.equal('Upgraded')
+    vm.$destroy()
+  })
+})
